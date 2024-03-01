27312ff138d1b36badb0e9a8cd49c91eaf1fdedd
Bill Garrett <garrett@astralfrontier.org>
2024-03-01 15:42:50 -0800

    Show a list of users and portals

--- src/components.rs
+++ src/components.rs
@@ -1,40 +1,73 @@
 use leptos::*;
 
+#[derive(Debug, Clone)]
+struct MapUser {
+    id: i32,
+    name: String,
+    maps_opened: i32,
+    maps_held: i32,
+}
+
+#[derive(Debug, Clone)]
+struct MapPortal {
+    id: i32,
+    name: String,
+    floors: i32,
+}
+
+#[component]
+pub fn MapUserView() -> impl IntoView {}
+
 #[component]
 pub fn App() -> impl IntoView {
-    let (count, set_count) = create_signal(0);
-    let double_count = move || count() * 2;
+    let (users, set_users) = create_signal(vec![MapUser {
+        id: 0,
+        name: String::from("New User"),
+        maps_opened: 0,
+        maps_held: 0,
+    }]);
+    let (portals, set_portals) = create_signal(vec![MapPortal {
+        id: 0,
+        name: String::from("New Portal"),
+        floors: 0,
+    }]);
 
     view! {
         <div class:container=true>
-            <button
-                on:click=move |_| {
-                    set_count.update(|n| *n += 1);
-                }
-
-                class:button=true
-                class=("is-primary", move || { count() < 25 })
-                disabled=move || { count() > 24 }
-            >
-                // on stable, this is move || count.get();
-                "Clickin' chicken: "
-                {move || count()}
-            </button>
-            <button
-                on:click=move |_| { set_count(0) }
-
-                class:button=true
-                class=("is-primary", move || { count() > 24 })
-            >
-                "Reset"
-            </button>
-            <br/>
-            <progress
-                max="50"
-                // signals are functions, so `value=count` and `value=move || count.get()`
-                // are interchangeable.
-                value=double_count
-            ></progress>
+            <div class:columns=true>
+                <div class:column=true class=("is-6", true)>
+                    <div class:columns=true>
+                        <div class:column=true class=("is-6", true)><strong>Player</strong></div>
+                        <div class:column=true class=("is-3", true)><strong>Maps</strong></div>
+                    </div>
+                    <For
+                        each=users
+                        key=|map_user| format!("{}-{}-{}-{}", map_user.id.clone(), map_user.name.clone(), map_user.maps_opened.clone(), map_user.maps_held.clone())
+                        let:map_user
+                    >
+                        <div class:columns=true>
+                            <div class:column=true class=("is-8", true)>{map_user.name}</div>
+                            <div class:column=true class=("is-4", true)>{map_user.maps_opened}/{map_user.maps_held}</div>
+                        </div>
+                    </For>
+                </div>
+                <div class:column=true class=("is-6", true)>
+                    <div class:columns=true>
+                        <div class:column=true class=("is-8", true)><strong>Portal</strong></div>
+                        <div class:column=true class=("is-4", true)><strong>Floors</strong></div>
+                    </div>
+                    <For
+                        each=portals
+                        key=|portal| format!("{}-{}-{}", portal.id.clone(), portal.name.clone(), portal.floors.clone())
+                        let:portal
+                    >
+                        <div class:columns=true>
+                            <div class:column=true class=("is-8", true)>{portal.name}</div>
+                            <div class:column=true class=("is-4", true)>{portal.floors}</div>
+                        </div>
+                    </For>
+                </div>
+            </div>
         </div>
     }
 }
