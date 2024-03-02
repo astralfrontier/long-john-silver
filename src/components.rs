a5db89bdc6017e88413370adbbd590764b71421f
Bill Garrett <garrett@astralfrontier.org>
2024-03-01 16:22:13 -0800

    Support adding and removing players and portals

--- src/components.rs
+++ src/components.rs
@@ -12,6 +12,7 @@ struct MapUser {
 struct MapPortal {
     id: i32,
     name: String,
+    opened_by: String,
     floors: i32,
 }
 
@@ -29,6 +30,7 @@ pub fn App() -> impl IntoView {
     let (portals, set_portals) = create_signal(vec![MapPortal {
         id: 0,
         name: String::from("New Portal"),
+        opened_by: String::from("Unknown"),
         floors: 0,
     }]);
 
@@ -39,15 +41,52 @@ pub fn App() -> impl IntoView {
                     <div class:columns=true>
                         <div class:column=true class=("is-6", true)><strong>Player</strong></div>
                         <div class:column=true class=("is-3", true)><strong>Maps</strong></div>
+                        <div class:column=true class=("is-3", true)><strong>Actions</strong></div>
                     </div>
                     <For
                         each=users
                         key=|map_user| format!("{}-{}-{}-{}", map_user.id.clone(), map_user.name.clone(), map_user.maps_opened.clone(), map_user.maps_held.clone())
                         let:map_user
                     >
-                        <div class:columns=true>
-                            <div class:column=true class=("is-8", true)>{map_user.name}</div>
-                            <div class:column=true class=("is-4", true)>{map_user.maps_opened}/{map_user.maps_held}</div>
+                        <div class:columns=true class=("is-vcentered", true)>
+                            <div class:column=true class=("is-6", true)>
+                                <nav class:level=true>
+                                    <div class=("level-left", true)>
+                                        <div class=("level-item", true)>
+                                            <button
+                                                class:delete=true
+                                                class=("is-small", true)
+                                                on:click=move |_| {
+                                                    set_users.update(|users| {
+                                                        users.retain(|u| u.id != map_user.id)
+                                                    })
+                                                }
+                                            ></button>
+                                        </div>
+                                        <div class=("level-item", true)>
+                                            {map_user.name.clone()}
+                                        </div>
+                                    </div>
+                                </nav>
+                            </div>
+                            <div class:column=true class=("is-3", true)>{map_user.maps_opened}/{map_user.maps_held}</div>
+                            <div class:column=true class=("is-3", true)>
+                                <button
+                                    class:button=true
+                                    class=("is-small", true)
+                                    class=("is-link", true)
+                                    on:click=move |_| {
+                                        set_portals.update(|portals| portals.push(MapPortal {
+                                            id: portals.iter().fold(std::i32::MIN, |a,b| a.max(b.id)) + 1,
+                                            name: String::from("New Portal"),
+                                            opened_by: map_user.name.clone(),
+                                            floors: 0
+                                        }))
+                                    }
+                                >
+                                    Portal
+                                </button>
+                            </div>
                         </div>
                     </For>
                     <button
@@ -55,7 +94,7 @@ pub fn App() -> impl IntoView {
                         class=("is-primary", true)
                         on:click=move |_| {
                             set_users.update(|users| users.push(MapUser {
-                                id: i32::try_from(users.len()).unwrap(),
+                                id: users.iter().fold(std::i32::MIN, |a,b| a.max(b.id)) + 1,
                                 name: String::from("New Player"),
                                 maps_opened: 0,
                                 maps_held: 0
@@ -67,8 +106,10 @@ pub fn App() -> impl IntoView {
                 </div>
                 <div class:column=true class=("is-6", true)>
                     <div class:columns=true>
-                        <div class:column=true class=("is-8", true)><strong>Portal</strong></div>
-                        <div class:column=true class=("is-4", true)><strong>Floors</strong></div>
+                        <div class:column=true class=("is-3", true)><strong>Portal</strong></div>
+                        <div class:column=true class=("is-3", true)><strong>Opened By</strong></div>
+                        <div class:column=true class=("is-3", true)><strong>Floors</strong></div>
+                        <div class:column=true class=("is-3", true)><strong>Actions</strong></div>
                     </div>
                     <For
                         each=portals
@@ -76,8 +117,30 @@ pub fn App() -> impl IntoView {
                         let:portal
                     >
                         <div class:columns=true>
-                            <div class:column=true class=("is-8", true)>{portal.name}</div>
-                            <div class:column=true class=("is-4", true)>{portal.floors}</div>
+                            <div class:column=true class=("is-3", true)>
+                            <nav class:level=true>
+                            <div class=("level-left", true)>
+                                <div class=("level-item", true)>
+                                    <button
+                                        class:delete=true
+                                        class=("is-small", true)
+                                        on:click=move |_| {
+                                            set_portals.update(|portals| {
+                                                portals.retain(|p| p.id != portal.id)
+                                            })
+                                        }
+                                    ></button>
+                                </div>
+                                <div class=("level-item", true)>
+                                    {portal.name.clone()}
+                                </div>
+                            </div>
+                        </nav>
+                            </div>
+                            <div class:column=true class=("is-3", true)>{portal.opened_by}</div>
+                            <div class:column=true class=("is-3", true)>{portal.floors}</div>
+                            <div class:column=true class=("is-3", true)>
+                            </div>
                         </div>
                     </For>
                 </div>
